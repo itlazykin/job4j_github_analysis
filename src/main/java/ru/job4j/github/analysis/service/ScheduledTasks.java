24d587e43252fcc0c6915a8509dda0598393b29d
itlazykin <dzaka89@gmail.com>
2025-02-11 11:28:47 +0300

    Правки сервисов.

--- src/main/java/ru/job4j/github/analysis/service/ScheduledTasks.java
+++ src/main/java/ru/job4j/github/analysis/service/ScheduledTasks.java
@@ -6,24 +6,30 @@ import org.springframework.stereotype.Service;
 import ru.job4j.github.analysis.model.Commit;
 import ru.job4j.github.analysis.model.Repository;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Service
 public class ScheduledTasks {
 
     private final RepositoryService repositoryService;
+    private final CommitService commitService;
     private final GitHubService gitHubService;
 
     @Scheduled(fixedRateString = "${scheduler.fixedRate}")
     public void fetchCommits() {
-        var repositories = repositoryService.findAll();
+        List<Repository> repositories = repositoryService.getAllRepositories();
         for (Repository repository : repositories) {
-            var latestCommit = repositoryService.findLatestCommit(repository);
-            var lastCommitSha = latestCommit != null ? latestCommit.getSha() : null;
-            var commits = gitHubService.fetchNewCommits(repository.getUserName(), repository.getName(), lastCommitSha);
+            Commit lastCommit = commitService.findLastCommitByRepository(repository);
+            String sha = lastCommit != null ? lastCommit.getSha() : null;
+            List<Commit> commits = gitHubService.fetchCommits(
+                    repository.getUsername(),
+                    repository.getName(),
+                    sha
+            );
             if (!commits.isEmpty()) {
                 for (Commit commit : commits) {
-                    commit.setRepository(repository);
-                    repositoryService.create(commit);
+                    commitService.create(commit);
                 }
             }
         }
