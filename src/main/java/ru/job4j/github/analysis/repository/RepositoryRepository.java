8bd602a8de701e7cd15dec8ce01be5d5be4d84c2
itlazykin <dzaka89@gmail.com>
2025-02-11 11:26:51 +0300

    Правки репозиториев.

--- src/main/java/ru/job4j/github/analysis/repository/RepositoryRepository.java
+++ src/main/java/ru/job4j/github/analysis/repository/RepositoryRepository.java
@@ -3,12 +3,5 @@ package ru.job4j.github.analysis.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.github.analysis.model.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface RepositoryRepository extends JpaRepository<Repository, Long> {
-
-    List<Repository> findAll();
-
-    Optional<Repository> findByName(String name);
 }
